d552dd9f16f7ace8918457c761c7d45d301559de
Sam Deng <SamDeng@gmail.com>
2011-10-15 18:55:27 -0400

    Data extraction from CSVs and storing to MongoDB.

--- src/api/optimus.js
+++ src/api/optimus.js
@@ -1,14 +1,68 @@
 var csv = require('./node_modules/csv');
-var csv_file = '/opt/collectd/var/lib/collectd/csv/localhost/memory/memory-free-2011-10-15';
+var mongoose = require('mongoose');
 
-csv()
-.fromPath(csv_file)
-.transform(function(data) {
-    data.unshift(data.pop());
-    return data;
-})
-.on('data', function(data, index){
-    console.log('Bytes Free: ' +data[0]);
+
+mongoose.connect('mongodb://localhost/vpsFlow_db');
+// Embedded document model
+var Schema = mongoose.Schema;
+var Stat = mongoose.Schema;
+
+var Stat = new Schema({
+    timestamp: {type: Number},
+    type: {type: String},
+    value: {type: Number}
 });
+
+var Server = new Schema({
+    stats: [Stat]
+});
+
+var ServerModel = mongoose.model('Server', Server);
+var server = new ServerModel();
+
+
+// Read CSV log
+var csv_files = [
+    '/opt/collectd/var/lib/collectd/csv/localhost/memory/memory-free-2011-10-15',
+    '/opt/collectd/var/lib/collectd/csv/localhost/memory/memory-used-2011-10-15',
+    '/opt/collectd/var/lib/collectd/csv/localhost/memory/memory-cached-2011-10-15',
+    '/opt/collectd/var/lib/collectd/csv/localhost/memory/memory-buffered-2011-10-15'
+]
+
+csv_files.forEach(function(csv_file) {
+    // Extract name of stat
+    segments = csv_file.split('/');
+    stat_type = segments[segments.length-1];
+    stat_type_segments = stat_type.split('-');
+    stat_type = stat_type_segments[0] + ' ' + stat_type_segments[1];
+
+    csv()
+    .fromPath(csv_file)
+    .transform(function(data) {
+        data.unshift(data.pop());
+        return data;
+    })
+    .on('data', function(data, index){
+        if (data[1] == "epoch") {
+            return;
+        }
+        console.log(data[1]);
+        server.stats.push({
+            'timestamp': data[1],
+            'type': stat_type,
+            'value': data[0]
+        });
+        console.log('Right before the save...');
+        server.save(function(err) {
+            console.log('Attempting to save...');
+            if (!err) {
+                console.log('No error.');
+            } else {
+                console.log('Error!');
+            }
+        });
+    });
+});
+
 
 
